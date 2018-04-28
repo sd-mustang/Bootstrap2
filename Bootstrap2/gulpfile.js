29b27404ed023e01354f40cd52c9ff8c64c56cde
Joshua Broadbooks <josh.broadbooks@gmail.com>
2018-04-28 10:42:17 -0500

    Add project files.

---
+++ Bootstrap2/gulpfile.js
@@ -0,0 +1,15 @@
+﻿var gulp = require("gulp"),
+    cssmin = require("gulp-cssmin"),
+    less = require("gulp-less");
+
+gulp.task("less", function () {
+    return gulp.src('Styles/main.less')
+        .pipe(less())
+        .pipe(gulp.dest('wwwroot/css'));
+})
+
+gulp.task("min:css", function () {
+    return gulp.src('wwwroot/css/main.css')
+        .pipe(cssmin())
+        .pipe(gulp.dest('wwwroot/css/minified/main.min.css'));
+})
